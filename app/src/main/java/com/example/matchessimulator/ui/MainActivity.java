4a9478f339bf44bff01107d564928cd32273dd23
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-06-21 12:53:39 -0300

    Consumindo a API

--- app/src/main/java/com/example/matchessimulator/ui/MainActivity.java
+++ app/src/main/java/com/example/matchessimulator/ui/MainActivity.java
@@ -2,14 +2,28 @@
 package com.example.matchessimulator.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.matchessimulator.R;
+import com.example.matchessimulator.data.GithubMatchesApi;
 import com.example.matchessimulator.databinding.ActivityMainBinding;
+import com.example.matchessimulator.domain.Match;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private GithubMatchesApi matchesApi;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,15 +32,41 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setupHttpClient();
         setupMatchesList();
         setupMatchesRefresh();
         setupFloatingActionButton();
     }
 
+    private void setupHttpClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://lucas-kobayashi.github.io/Matches-Simulator-API/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        matchesApi = retrofit.create(GithubMatchesApi.class);
+    }
+
 
     private void setupMatchesList() {
-    //TODO Listar partidas consumindo a nossa API
+        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+            @Override
+            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                if(response.isSuccessful()){
+                    List<Match> matches = response.body();
+                    Log.i("SIMULATOR", "Deu tudo certo, voltaram partidas: " + matches.size());
+                }else {
+                    showErrorMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Match>> call, Throwable t) {
+                showErrorMessage();
+            }
+        });
     }
+
 
     private void setupMatchesRefresh() {
 //        TODO Atualizar as partidas na ação do swipe.
@@ -34,5 +74,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupFloatingActionButton() {
 //        TODO Criar evento de click e simulação de partidas.
+    }
+
+    private void showErrorMessage() {
+        Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_LONG).show();
     }
 }
