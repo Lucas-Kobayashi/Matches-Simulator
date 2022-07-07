69ecc67d7763d4faf57db70589e7bb8ea7ef68a3
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-07-06 23:02:06 -0300

    Todas as func prontas

--- app/src/main/java/com/example/matchessimulator/ui/DetailActivity.kt
+++ app/src/main/java/com/example/matchessimulator/ui/DetailActivity.kt
@@ -3,7 +3,9 @@ package com.example.matchessimulator.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.matchessimulator.databinding.ActivityDetailBinding
+import com.example.matchessimulator.domain.Match
 
 class DetailActivity : AppCompatActivity() {
 
@@ -21,5 +23,28 @@ class DetailActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        loadMatchFromExtra()
+    }
+    private fun loadMatchFromExtra() {
+        intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let {
+            Glide.with(this).load(it.place.image).into(binding.ivPlace)
+            supportActionBar?.title = it.place.name
+
+            binding.tvDescription.text = it.description
+            Glide.with(this).load(it.homeTeam.image).into(binding.ivHomeTeam)
+            binding.tvHomeTeamName.text = it.homeTeam.name
+            binding.rbHomeTeamStars.rating = it.homeTeam.stars.toFloat()
+            if (it.homeTeam.score != null){
+                binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
+            }
+
+            Glide.with(this).load(it.awayTeam.image).into(binding.ivAwayTeam)
+            binding.tvAwayTeamName.text = it.awayTeam.name
+            binding.rbAwayTeamStars.rating = it.awayTeam.stars.toFloat()
+            if (it.awayTeam.score != null){
+                binding.tvAwayTeamScore.text = it.awayTeam.score.toString()
+            }
+        }
     }
 }
